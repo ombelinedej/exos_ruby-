743892e811ed77638ddf79935b3b8db297d50506
ombelinedej <ombeline.dj@gmail.com>
2019-10-02 00:08:11 +0200

    add exercise 21

---
+++ exo_21.rb
@@ -0,0 +1,25 @@
+
+puts "Combien d'étages veux-tu ?"
+
+number = gets.chomp.to_i
+
+number.times do |i|
+
+
+
+    number.times do |j|
+    		print " "
+
+
+end
+
+
+	number = number - 1
+
+    puts "#" * i
+
+ 
+ end
+
+
+
